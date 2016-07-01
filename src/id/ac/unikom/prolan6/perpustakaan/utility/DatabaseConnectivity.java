898672e0d34d5aad57cd538458cace9a814d9b5c
Rizal Yogi Pratama <Rizal Yogi Pratama>
2016-07-01 16:14:28 +0700

    Blank Project

---
+++ src/id/ac/unikom/prolan6/perpustakaan/utility/DatabaseConnectivity.java
@@ -0,0 +1,28 @@
+
+
+package id.ac.unikom.prolan6.perpustakaan.utility;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnectivity {
+    
+    private static Connection conn;
+    
+    private final static String url = "jdbc:mysql://localhost:3306/perpustakaan";
+    private final static String user  = "root";
+    private final static String password = "";
+    
+    public static Connection getConnection() {
+        try {
+            DriverManager.registerDriver(new Driver());
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            System.out.println("Koneksi database gagal dengan pesan : " + ex.getMessage());
+        }
+        return conn;
+    }
+}
