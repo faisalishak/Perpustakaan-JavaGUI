2815ca82416ef312f374b21a0f6b61da6d5e241f
Rizal Yogi Pratama <Rizal Yogi Pratama>
2016-07-03 14:19:00 +0700

    Peminjaman

---
+++ src/id/ac/unikom/prolan6/perpustakaan/enitiy/Anggota.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.unikom.prolan6.perpustakaan.enitiy;
+
+/**
+ *
+ * @author Robi Tanzil
+ */
+public class Anggota {
+
+    private int idAnggota;
+    private String nama;
+
+    public int getIdAnggota() {
+        return idAnggota;
+    }
+
+    public void setIdAnggota(int idAnggota) {
+        this.idAnggota = idAnggota;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+}
